18ff2aa6123ab3ef84c510c6796c9f7e8f848403
willthamic <will.hamic@gmail.com>
2018-05-29 16:27:49 -0500

    Added core functionality

--- Vector/Vector.cs
+++ Vector/Vector.cs
@@ -4,9 +4,9 @@ namespace Vector
 {
     public class Vector3
     {
-        float x = 0;
-        float y = 0;
-        float z = 0;
+        float x;
+        float y;
+        float z;
 
         public Vector3 (float x0, float y0, float z0)
         {
@@ -24,6 +24,46 @@ namespace Vector
         {
             float magnitude = this.Magnitude();
             return new Vector3(x / magnitude, y / magnitude, z / magnitude);
+        }
+
+        public static Vector3 Sum (Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.x + b.x, a.y + b.y, a.z + b.z);
+        }
+
+        public static Vector3 Subtract (Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.x - b.x, a.y - b.y, a.z - b.z);
+        }
+
+        public static Vector3 Scale (Vector3 a, float b)
+        {
+            return new Vector3(a.x * b, a.y * b, a.z * b);
+        }
+
+        public static float Dot (Vector3 a, Vector3 b)
+        {
+            return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+        }
+
+        public static Vector3 operator +(Vector3 a, Vector3 b)
+        {
+            return Vector3.Sum(a, b);
+        }
+
+        public static Vector3 operator -(Vector3 a, Vector3 b)
+        {
+            return Vector3.Subtract(a, b);
+        }
+
+        public static Vector3 operator *(Vector3 a, float b)
+        {
+            return Scale(a, b);
+        }
+
+        public static Vector3 operator *(float a, Vector3 b)
+        {
+            return Scale(b, a);
         }
     }
 }
