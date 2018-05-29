cd99c780a5b6b03228054c65f92b8e90eec4b9e3
willthamic <will.hamic@gmail.com>
2018-05-29 17:44:25 -0500

    Moved cross product method over from wraycast project and fixed it.

--- Vector/Vector.cs
+++ Vector/Vector.cs
@@ -4,9 +4,9 @@ namespace Vector
 {
     public class Vector3
     {
-        public float x;
-        public float y;
-        public float z;
+        float x;
+        float y;
+        float z;
 
         public Vector3 (float x0, float y0, float z0)
         {
@@ -24,6 +24,21 @@ namespace Vector
         {
             float magnitude = this.Magnitude();
             return new Vector3(x / magnitude, y / magnitude, z / magnitude);
+        }
+
+        public static Vector3 Normal(Vector3 u, Vector3 v)
+        {
+            float u1 = u.x;
+            float u2 = u.y;
+            float u3 = u.z;
+            float v1 = v.x;
+            float v2 = v.y;
+            float v3 = v.z;
+
+            float x = u2 * v3 - v2 * u3;
+            float y = v1 * u3 - u1 * v3;
+            float z = u1 * v2 - v1 * u2;
+            return new Vector3(x, y, z).Unit();
         }
 
         public String ToString ()
